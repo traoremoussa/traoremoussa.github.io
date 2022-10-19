9914fc7846aeb623bdd39e5a3a778ef43d6fdaea
traore <moussa.traoreinfo@gmail.com>
2022-10-19 05:17:08 +0200

    fix-fist tim

--- src/app/portfolio/footer/footer.component.ts
+++ src/app/portfolio/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  linkedin!:string;
   constructor() { }
 
   ngOnInit(): void {
+    this.linkedin="https://www.linkedin.com/in/moussa-m-mah-traore-95a873115/"
   }
 
 }
