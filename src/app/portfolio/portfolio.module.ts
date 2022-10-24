d898084361aa29211bbd61d88d51396220cf6802
traore <moussa.traoreinfo@gmail.com>
2022-10-25 00:16:04 +0200

    test deployement

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -9,7 +9,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './portfolio.component';
 import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 // à connaitre
 import { NgxPaginationModule } from 'ngx-pagination';
 import { ServicesComponent } from './services/services.component';
@@ -38,7 +38,7 @@ import { IntroComponent } from './intro/intro.component';
     FormsModule,
     NgxPaginationModule,
     PortfolioRoutingModule,
-    FontAwesomeModule
+
   ],
 
 })
