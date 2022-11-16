ada464a89d571ae297e965eab1860bc7360b9753
traore <moussa.traoreinfo@gmail.com>
2022-11-16 14:50:54 +0100

    [feature] modif message

--- src/app/portfolio.service.ts
+++ src/app/portfolio.service.ts
@@ -15,25 +15,29 @@ skillsData: any = [
     'id': '1',
     'skill': 'HTML',
     'progress': '70%',
-    'icon': 'fab fa-html5'
+    'icon': 'fab fa-html5',
+    'exp':'Plus de 3 ans d\'utilisation avec Html5 et developpement web'
   },
   {
     'id': '2',
     'skill': 'CSS3',
     'progress': '80%',
-    'icon': 'fab fa-css3-alt'
+    'icon': 'fab fa-css3-alt',
+    'exp':'Plus de 3 ans avec Style css3 + html, js, developpement web'
   },
   {
     'id': '3',
     'skill': 'JAVASCRIPT JQUERY',
     'progress': '80%',
-    'icon': 'fab fa-js'
+    'icon': 'fab fa-js',
+    'exp':'Plus de 2 ans d\'utilisation,JS et ses framwork Angular dev web'
   },
   {
     'id': '4',
     'skill': 'JAVA,J2EE',
     'progress': '75%',
-    'icon': 'fab fa-java'
+    'icon': 'fab fa-java',
+    'exp':'Plus de 3 ans d\'utilisation,java,J2EE, spring, swing, Rest, dev web'
   }
 ];
 
