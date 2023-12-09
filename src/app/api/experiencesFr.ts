f6de7838be277abb978f1965a9eb34a8756d3db2
traore <moussa.traoreinfo@gmail.com>
2023-12-09 11:56:37 +0100

    modif cv link

--- src/app/api/experiencesFr.ts
+++ src/app/api/experiencesFr.ts
@@ -1,91 +1,94 @@
 export const experiencesFr = [
-    {
-        title: "Software Developer",
-        company: 'Cat-amania',
-        period: "Depuis Avril 2023",
-        technologies: 'Spring/Angular, Java, Swing, Service(Rest,SOAP), Devops(Maven, Git/Gitlab, Jenkins), Oracle, Sql-Server, Tomcat, Hibernate, Jira, Junit, eclipse ',
-        missions: [
-            "AirFrance-KLM",
-            "Application pour la gestion de comptabilité",
-            "Conception, Maitenance, Evolution",
-            "Utilisation de la Methodologie Agile(Scrum)"
-        ],
-        detailIsDisplayed: false,
-    }
   {
-      title: "Développeur Java",
-      company: 'Berger-Levrault',
-      period: "Depuis sept 2020",
-      technologies: 'Java, Swing, Service(Rest,SOAP), Devops(Maven, Git/Gitlab, Jenkins), Oracle, Sql-Server, Tomcat, Hibernate, Jira, Junit, eclipse ',
-      missions: [
-          "Application pour la gestion de comptabilité",
-          "Conception, Maitenance, Evolution",
-          "Utilisation de la Methodologie Agile(Scrum)"
-      ],
-      detailIsDisplayed: false,
-  },
-  {
-      title: "Développeur Java/ Stage de 8 mois",
-      company: 'Berger-Levrault',
-      period: "nov 2019 - juin 2020",
-      technologies: 'JAVA 8, Web-service, Swing, Spring, Maven, Jenkins, TesLink, Jira, Junit, eclipse ',
-      missions: [
-          "Rédiger les jeux de tests automatisés",
-          "Participer à la rédaction de scénarii de tests (TDD)",
-          "Participer à la documentation utilisateur et technique"
-      ],
-      detailIsDisplayed: false,
-  },
-  {
-      title: "Développeur Full-Stack",
-      company: 'Capgemini',
-      period: "mai 2019 - août 2019",
-      technologies: 'Java, Python, SQL, PySpark, Spark-Sql,JavaScript, HTML, CSS',
-      missions: [
-          "Plate-forme Skywise est le programme Big Data & Analytics d'Airbus Group lancé en 2017",
-          "Développement Back-end (Big Data)",
-          "Développement Front-end (Dashboard de visualisation des données)",
-          "Tests Unitaires, Tests d’intégrations",
-          "Utilisation des Méthodes Agile : SAFe et SCRUM",
-      ],
-      detailIsDisplayed: false,
-  },
-  {
-      title: "Développeur Mobile",
-      company: 'IRT de Paul sabatier',
-      period: "Jan 2019 - Mai 2019",
-      technologies: 'Java, PHP, Android-studio, MySQL, Github, Git, CodeIgniter.',
-      missions: [
-          "Application TheSnowBook apprentissage numérique du snowboard.",
-          "Intégration des nouvelles fonctionnalités",
-          "Tests Unitaires, Tests d’intégrations",
-          "Utilisation méthode agile SCRUM"
-      ],
-      detailIsDisplayed: false,
-  },
-  {
-    title: "Développeur web",
-    company: 'IRT de Paul sabatier',
-    period: "Mars 2017 - Mai 2017",
-    technologies: 'PHP, Symfony, JavaScript, GiTlab, Git, MySQL, Html5, CSS3.',
+    title: 'Software Developer',
+    company: 'Cat-amania',
+    period: 'Depuis Avril 2023',
+    technologies:
+      'Spring/Angular, Java, Swing, Service(Rest,SOAP), Devops(Maven, Git/Gitlab, Jenkins), Oracle, Sql-Server, Tomcat, Hibernate, Jira, Junit, eclipse ',
     missions: [
-        "Application checkyoursmile apprentissage des langues.",
-        "Intégration des nouvelles fonctionnalités",
-        "Developpement du FlashCard"
+      'AirFrance-KLM',
+      'Application pour la gestion de comptabilité',
+      'Conception, Maitenance, Evolution',
+      'Utilisation de la Methodologie Agile(Scrum)',
     ],
     detailIsDisplayed: false,
-},
-{
-  title: "Développeur Web/Mobile",
-  company: 'Université Guelma (Algerie)',
-  period: "Jan 2016 - Mai 2016",
-  technologies: 'Java, PHP, Android-studio, MySQL, Github, Git',
-  missions: [
+  },
+  {
+    title: 'Développeur Java',
+    company: 'Berger-Levrault',
+    period: 'Depuis sept 2020',
+    technologies:
+      'Java, Swing, Service(Rest,SOAP), Devops(Maven, Git/Gitlab, Jenkins), Oracle, Sql-Server, Tomcat, Hibernate, Jira, Junit, eclipse ',
+    missions: [
+      'Application pour la gestion de comptabilité',
+      'Conception, Maitenance, Evolution',
+      'Utilisation de la Methodologie Agile(Scrum)',
+    ],
+    detailIsDisplayed: false,
+  },
+  {
+    title: 'Développeur Java/ Stage de 8 mois',
+    company: 'Berger-Levrault',
+    period: 'nov 2019 - juin 2020',
+    technologies:
+      'JAVA 8, Web-service, Swing, Spring, Maven, Jenkins, TesLink, Jira, Junit, eclipse ',
+    missions: [
+      'Rédiger les jeux de tests automatisés',
+      'Participer à la rédaction de scénarii de tests (TDD)',
+      'Participer à la documentation utilisateur et technique',
+    ],
+    detailIsDisplayed: false,
+  },
+  {
+    title: 'Développeur Full-Stack',
+    company: 'Capgemini',
+    period: 'mai 2019 - août 2019',
+    technologies: 'Java, Python, SQL, PySpark, Spark-Sql,JavaScript, HTML, CSS',
+    missions: [
+      "Plate-forme Skywise est le programme Big Data & Analytics d'Airbus Group lancé en 2017",
+      'Développement Back-end (Big Data)',
+      'Développement Front-end (Dashboard de visualisation des données)',
+      'Tests Unitaires, Tests d’intégrations',
+      'Utilisation des Méthodes Agile : SAFe et SCRUM',
+    ],
+    detailIsDisplayed: false,
+  },
+  {
+    title: 'Développeur Mobile',
+    company: 'IRT de Paul sabatier',
+    period: 'Jan 2019 - Mai 2019',
+    technologies: 'Java, PHP, Android-studio, MySQL, Github, Git, CodeIgniter.',
+    missions: [
+      'Application TheSnowBook apprentissage numérique du snowboard.',
+      'Intégration des nouvelles fonctionnalités',
+      'Tests Unitaires, Tests d’intégrations',
+      'Utilisation méthode agile SCRUM',
+    ],
+    detailIsDisplayed: false,
+  },
+  {
+    title: 'Développeur web',
+    company: 'IRT de Paul sabatier',
+    period: 'Mars 2017 - Mai 2017',
+    technologies: 'PHP, Symfony, JavaScript, GiTlab, Git, MySQL, Html5, CSS3.',
+    missions: [
+      'Application checkyoursmile apprentissage des langues.',
+      'Intégration des nouvelles fonctionnalités',
+      'Developpement du FlashCard',
+    ],
+    detailIsDisplayed: false,
+  },
+  {
+    title: 'Développeur Web/Mobile',
+    company: 'Université Guelma (Algerie)',
+    period: 'Jan 2016 - Mai 2016',
+    technologies: 'Java, PHP, Android-studio, MySQL, Github, Git',
+    missions: [
       "Mise en place d'une application de commande taxi",
       "conception et le developpement de l'application web",
       "conception et le developpement pour l'admin",
-      "conception et le developpement pour le mobile"
-  ],
-  detailIsDisplayed: false,
-},
+      'conception et le developpement pour le mobile',
+    ],
+    detailIsDisplayed: false,
+  },
 ];
